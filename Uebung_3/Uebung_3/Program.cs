34479a8dd0dc9776dd699ea4f6f1fe7f5051d7ec
Ilka77 <61781492+Ilka77@users.noreply.github.com>
2020-04-03 14:15:45 +0200

    Startprogramm von Ilka
    start

--- Uebung_3/Uebung_3/Program.cs
+++ Uebung_3/Uebung_3/Program.cs
@@ -10,6 +10,47 @@ namespace Uebung_3
     {
         static void Main(string[] args)
         {
+            //  Schreiben Sie eine Eingabe für die Kantenlängen a und b
+            Console.WriteLine("Bitte geben Sie Kantenlänge a (Breite) an");
+            string eingabeA;
+            eingabeA = Console.ReadLine();
+            double a;
+            a = Convert.ToDouble(eingabeA);
+
+            Console.WriteLine("Bitte geben Sie Kantenlänge b (Höhe) an");
+            string eingabeB;
+            eingabeB = Console.ReadLine();
+            double b;
+            b = Convert.ToDouble(eingabeB);
+
+
+            //• Berechnen Sie die Fläche
+            double flaeche;
+            flaeche = a * b;
+            Console.WriteLine("Die FLäche beträgt: " + flaeche + " qm");
+
+            
+            //• Berechnen Sie den Flächenschwerpunkt des Profils.
+            double schwerpunktX;
+            double schwerpunktY;
+
+            schwerpunktX = a / 2;
+            schwerpunktY = b / 2;
+
+            Console.WriteLine("Schwerpunkt (X/Y): (" + schwerpunktX + "/" + schwerpunktY + ")");
+
+            //• Berechnen Sie die Flächenträgheitsmomente des Profils
+            double ix;
+            double iy;
+
+            ix = a * Math.Pow(b, 3) / 12;
+            iy = b * Math.Pow(a, 3) / 12;
+
+            Console.WriteLine("Ix: " + ix + " mm^4");
+            Console.WriteLine("Iy: " + iy + " mm^4");
+
+
+            Console.ReadKey();
         }
     }
 }
