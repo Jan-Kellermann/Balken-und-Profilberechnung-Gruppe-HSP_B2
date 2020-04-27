57c7f118a269a2a1ab3a8fb6db29b83928628ab6
Jan-Kellermann <61827316+Jan-Kellermann@users.noreply.github.com>
2020-04-27 21:08:50 +0200

    T-Profil erste berechnungen
    Erste Berechnungen des T-Profils und griederung mit Auswahl über switch

--- Uebung_3/Uebung_3/Program.cs
+++ Uebung_3/Uebung_3/Program.cs
@@ -15,7 +15,7 @@ namespace Uebung_3
 
 
 
-        static void Main(string[] args)
+        static void Main()
 
         {
 
@@ -30,11 +30,17 @@ namespace Uebung_3
             MessageBoxButtons buttons = MessageBoxButtons.YesNo;
             DialogResult result = MessageBox.Show(message, title, buttons);
 
-            if (result == DialogResult.No) 
+
+            Console.WriteLine("Für die berechnung eines einfachen Balkens 1 drücken, für einen T-Träger 2, für einen Kasten 3");
+
+            int nummer = Convert.ToInt32(Console.ReadLine());
+               int caseSwitch =  nummer;
+
+            switch(caseSwitch)
             {
-                System.Windows.Application.Exit();
-            }
-            //hier komm ich jetzt nicht mehr weiter wie schließt man das Programm bei drücken von nein?
+
+                case 1:
+
             //Schreiben Sie eine Eingabe für die Kantenlängen a und b
             Console.WriteLine("Bitte geben Sie Kantenlänge a (Breite) in mm an");
             string eingabeA;
@@ -50,16 +56,11 @@ namespace Uebung_3
 
 
             //• Berechnen Sie die Fläche
-            double flaeche = Flaechenberechnung(a, b);
+            double flaeche = Rechteck(a, b);
             Console.WriteLine("Die FLäche beträgt: " + flaeche + " qm");
 
             //• Berechnen Sie den Flächenschwerpunkt des Profils.
             Schwerpunktberechnung(a, b);
-            
-
-            
-
-           
 
             //• Berechnen Sie die Flächenträgheitsmomente des Profils
             double ix;
@@ -71,16 +72,93 @@ namespace Uebung_3
             Console.WriteLine("Ix: " + ix + " mm^4");
             Console.WriteLine("Iy: " + iy + " mm^4");
 
-
             Console.ReadKey();
+                    
+             break;
 
+                case 2:
+                    
+                    double bBreite;
+                    double bBreiteRip;
+                    double hHöhe;
+                    double hHöheRip;
+                    double lLänge;
+
+                    do
+
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("Hier kannst du einen T-Träger berechnen:");
+
+                        Console.WriteLine("Breite B: "); bBreite = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Rippenbreite b (die Rippe muss schmaler als die Gesamtbreite sein): "); bBreiteRip = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Höhe H: "); hHöhe = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Rippenhöhe h (die Rippe muss flacher als die Gesamthöhe sein): "); hHöheRip = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Länge L (die Rippe muss kürzer als die Gesamthöhe sein):"); lLänge = Convert.ToDouble(Console.ReadLine());
+
+                        if (hHöhe < hHöheRip && bBreite < bBreiteRip)
+                        {
+                            Console.WriteLine("Überprüfe deine Eingabe, die Rippe muss flacher als die Gesamthöhe sein, und die Rippe muss schmaler als die Gesamtbreite sein, bestätige mit einer Taste");
+
+                            Console.ReadKey();
+                        }
+
+                        else if (bBreite < bBreiteRip)
+                        {
+                            Console.WriteLine("Überprüfe deine Eingabe, die Rippe muss schmaler als die Gesamtbreite sein, bestätige mit einer Taste");
+
+                            Console.ReadKey();
+                        }
+
+                        else if (hHöhe < hHöheRip)
+                        {
+                            Console.WriteLine("Überprüfe deine Eingabe, die Rippe muss kürzer als die Gesamthöhe sein, bestätige mit einer Taste");
+
+                            Console.ReadKey();
+                        }
+
+                        else if (lLänge < 0)
+                        {
+                            Console.WriteLine("Überprüfe deine Eingabe, der Träger muss länger Null sein, bestätige mit einer Taste");
+
+                            Console.ReadKey();
+                        }
+
+                    }
+                    while (bBreite < bBreiteRip || hHöhe < hHöheRip || lLänge < 0);
+
+                        double Fläche = Rechteck(bBreite, hHöhe) + Rechteck(bBreiteRip, hHöheRip);
+                        double Volumen = Fläche * lLänge;
+
+                        Console.WriteLine("Die Fläche beträgt: " + Fläche);
+                        Console.WriteLine("Das Volumen beträgt: " + Volumen);
+
+                    //Komentare und weitere berechnungen folgen
+
+
+
+
+
+                    Console.ReadKey();
+                break;
+
+            //berechnung T-Profil
 
 
         }
-        public static double Flaechenberechnung(double a, double b)
+
+
+        }
+
+        public static double Rechteck(double a, double b)
         {
             double flaeche;
-            flaeche = a * b;            
+            flaeche = a * b;
             return flaeche;
         }
 
@@ -88,8 +166,14 @@ namespace Uebung_3
         {
             double schwerpunktX = a / 2;
             double schwerpunktY = b / 2;
-            Console.WriteLine("Schwerpunkt (X/Y): (" + schwerpunktX + "/" + schwerpunktY + ")");           
+            Console.WriteLine("Schwerpunkt (X/Y): (" + schwerpunktX + "/" + schwerpunktY + ")");
         }
+
+
+
+
+
+
 
     }
 }
